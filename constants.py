0a36e30856821953005b9b6936b3011ae257ff00
Yahor Karachun <yahor_karachun@epam.com>
2017-03-30 18:41:20 +0300

    Added constants module

---
+++ constants.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+KEYBOARD_TITLE = {
+	":add" : "Add me!",
+	":del" : "Remove me!"
+}
+
+ADD_COMMAND = ":add"
+DELETE_COMMAND = ":del"
+
+DATA_SEPARATOR = ":"
+TOKEN = "329092132:AAEJl4dlsv3rgZ8Q5OkrBLB0mTdCM7oyOzQ"
